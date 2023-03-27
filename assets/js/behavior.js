736f993dd28881b0413dd00149c7be0cafde1298
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-03-28 07:46:58 +0900

    マウスをかざす、離す、アンカーをクリックしたら別窓開く => addEventListener

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,47 +1,16 @@
-// 要素を取るやり方
-const os = document.getElementById("os")
-let options = os.children
-console.log(options)  // => HTMLCollection!!!
-// ここでのoptionsは『HTMLCollection』なので『forEach』は使えない。
-// options.forEach(opt => {
-//   console.log(opt.value)
-// })
+// 画像にマウスをかざしたら別画像に切り替える。
+const pic = document.getElementById("cover")
+pic.addEventListener("mouseenter", function() {
+  this.src = "../img/p2.jpg"
+}, false)
 
-for (let opt of options) {
-  console.log(opt.value)
-}
+// マウスが画像から別画像に切り替える。
+pic.addEventListener("mouseleave", function() {
+  this.src = "../img/p3.jpg"
+}, false)
 
-// -----
-
-// // Nodeを取るやり方　要素だけを取りたい場合に注意が必要。
-// const os = document.querySelector("#os")
-// let options = os.childNodes
-// // この場合は、改行と要素を取る。
-// // なので条件分けで要素だけを抽出する。
-// // なお、ここでのoptionsは『NodeList』なので『forEach』で回せる。
-
-// // forEach
-// console.log(options)
-// options.forEach(elem => {
-//   if (elem.nodeType === 1) {
-//     console.log(elem.value)
-// }
-// })
-
-// for
-// for (let opt of options) {
-//   // console.log(opt)
-//   if (opt.nodeType === 1) {
-//     console.log(opt.value)
-//   }
-// }
-
-// -----
-
-// // firstElementChild, nextElementSiblingプロパティを使うやり方
-// const os = document.getElementById("os")
-// let child = os.firstElementChild
-// while (child) {
-//   console.log(child.value)
-//   child = child.nextElementSibling
-// }
+// anchorをクリックしたら別窓でURLを開く。
+const link = document.getElementById("site")
+link.addEventListener("click", function() {
+  this.setAttribute("target", "_blank")
+}, false)
