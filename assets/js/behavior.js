c3bf3e98a0e59b0ba8fdc26ea309db8de47bc149
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-03-30 11:38:37 +0900

    removeAttribute => 属性値を削除する
    	topics:
    		=> 一つ上の要素を複数まとめて配列に入れてからfor ofで回す。
    		   最初の子要素にアクセスして任意の属性の有無で絞り込み、
    		   .removeAttributeで任意の属性を取り去る作戦。
    		=> gridでレイアウト。『居抜き』を設定してみた。

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,11 +1,9 @@
-// // #1 .hasAttribute()で条件分岐で抽出して.setAttributeで属性の設定
-const images = document.querySelectorAll("dd > img")
-for (img of images) {
-  if (!img.hasAttribute("src")) {
-    img.setAttribute("src", "../img/p1.jpg")
+// #1 .removeAttribute 属性を削除
+const items = document.querySelectorAll(".item")
+
+for (item of items) {
+  let target= item.firstElementChild
+  if (target.hasAttribute("disabled")) {
+    target.removeAttribute("disabled")
   }
 }
-    
-// #2 インデックスが分かっているならNamedNodeMapから直接設定。まぁ、ないな。
-// const images = document.querySelectorAll("dd > img")
-// images.item(1).setAttribute("src", "../img/p1.jpg")
