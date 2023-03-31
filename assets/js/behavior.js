ec6b8b89715add0a6f1691ff561bcd569eafc313
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-03-31 14:17:25 +0900

    要素の属性値を取得／設定する
    	=> 属性を設定する
    		.setAttributeを使う	this.setAttribute("target", "_blank")
    		属性を使う		this.target = "_blank"
    	=> 属性を参照する
    		.getAttributeを使う	email.getAttribute("value")
                    属性を使う          email.value
    	同じことができる。
    	留意しなければいけない点は、参照の場合に特に注意が必要で、
    	属性は現在の値、メソッドは初期値を使うということ。

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,9 +1,25 @@
-// #1 .removeAttribute 属性を削除
-const items = document.querySelectorAll(".item")
-
-for (item of items) {
-  let target= item.firstElementChild
-  if (target.hasAttribute("disabled")) {
-    target.removeAttribute("disabled")
+const images = document.querySelectorAll("img")
+for (image of images) {
+  if (!image.hasAttribute("src")) {
+    // image.setAttribute("src", "../img/p1.jpg")
+    image.src = "../img/p1.jpg"
   }
 }
+
+const links = document.querySelectorAll("dd > a")
+for (link of links)  {
+  if (!link.hasAttribute("target")) {
+    link.addEventListener("click", function() {
+      // this.setAttribute("target", "_blank")
+      this.target = "_blank"
+    }, false)
+  }
+}
+
+const email = document.getElementById("email")
+document.querySelector(".item.five > input").addEventListener("click", function() {
+  console.log(email.value)
+  console.log(email.getAttribute("value"))
+}, false)
+
+
