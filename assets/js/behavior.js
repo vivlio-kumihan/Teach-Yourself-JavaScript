10cd6df27706a125639dd1a17295e740a8cd9e50
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-04-01 15:38:37 +0900

    Nodeの生成と値の代入 => form, type="text"へ入力したインスタンスをListで表示させる。
    	formのtype="text"で入力したインスタンスの処理の流れ
    		textを入力すると、value属性のインスタンスがフワッと出来上がっている。
    		それを属性名のメソッドで変数に格納して運用する。
    	2つのやり方
    		要素を生成させて属性名のメソッドで形を整えていく方法。
    		ノードを生成させる方法。こちらには使ったことがないから苦手意識。
    	意味があるはず。何度も読むこと。

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,19 +1,30 @@
-let figs = document.querySelectorAll("figure")
+// createElement(), textContent, append()で要素の追加と属性の結び付け
 
-for (fig of figs) {
-  fig.addEventListener("mouseenter", function () {
-    console.log("enter!")
-    console.log(this.firstElementChild)
-    this.classList.add("border-none")
-    this.firstElementChild.classList.add("active")
-    this.lastElementChild.classList.add("disappear")
-  }, false)
-  
-  fig.addEventListener("mouseleave", function () {
-    console.log("leave!")
-    console.log(this.firstElementChild)
-    this.classList.remove("border-none")
-    this.firstElementChild.classList.remove("active")
-    this.lastElementChild.classList.remove("disappear")
-  }, false)
-}
+let title = document.getElementById("title")
+let url = document.getElementById("url")
+let bookList = document.getElementById("book-list")
+
+document.getElementById("btn").addEventListener("click", function() {
+  let li = document.createElement("li")
+  let anchor = document.createElement("a")
+  anchor.href = url.value
+  anchor.textContent = title.value
+  li.append(anchor)
+  bookList.append(li)
+}, false)
+
+// Nodeで要素の追加と属性の結び付け
+
+// let title = document.getElementById("title")
+// let url = document.getElementById("url")
+// let bookList = document.getElementById("book-list")
+
+// document.getElementById("btn").addEventListener("click", function() {
+//   let anchor = document.createElement("a")
+//   let href = document.createAttribute("href")
+//   href.value = url.value
+//   anchor.setAttributeNode(href)
+//   let text = document.createTextNode(title.value)
+//   anchor.append(text)
+//   bookList.append(anchor)
+// }, false)
