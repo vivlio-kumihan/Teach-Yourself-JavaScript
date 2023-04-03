f1b1fa5857366b4cbaacebced5f6a7569772c91d
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-04-03 10:01:51 +0900

    Nodeの生成と値の代入 => 空のulに連想配列の各値をliにレイアウトして表組を作成する。
    	・配列をfor ofで回すやり方は、描画環境のパフォーマンスを考えると控えるべき。
    	・対策として、
    		=> document.createDocumentFragmane()を使う。

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,30 +1,24 @@
-// createElement(), textContent, append()で要素の追加と属性の結び付け
+const books = [
+  { title: 'ゼロからわかる TypeScript入門', price: 2948 },
+  { title: 'Bootstrap 5の教科書', price: 3828 },
+  { title: 'はじめてのAndroidアプリ開発', price: 3520 },
+]
 
-let title = document.getElementById("title")
-let url = document.getElementById("url")
-let bookList = document.getElementById("book-list")
+const list = document.getElementById("list")
 
-document.getElementById("btn").addEventListener("click", function() {
+// 配列をfor ofで回すやり方は、描画環境のパフォーマンスを考えると控えるべき。
+// for (book of books) {
+//   let li = document.createElement("li")
+//   li.textContent = `${ book.title } => ${ book.price }円`
+//   list.append(li)
+// }
+
+// 対策 => document.createDocumentFragmane()を使う。
+let frag = document.createDocumentFragment()
+for (book of books) {
   let li = document.createElement("li")
-  let anchor = document.createElement("a")
-  anchor.href = url.value
-  anchor.textContent = title.value
-  li.append(anchor)
-  bookList.append(li)
-}, false)
+  li.textContent = `${ book.title } => ${ book.price }円`
+  frag.append(li)
+}
 
-// Nodeで要素の追加と属性の結び付け
-
-// let title = document.getElementById("title")
-// let url = document.getElementById("url")
-// let bookList = document.getElementById("book-list")
-
-// document.getElementById("btn").addEventListener("click", function() {
-//   let anchor = document.createElement("a")
-//   let href = document.createAttribute("href")
-//   href.value = url.value
-//   anchor.setAttributeNode(href)
-//   let text = document.createTextNode(title.value)
-//   anchor.append(text)
-//   bookList.append(anchor)
-// }, false)
+list.append(frag)
