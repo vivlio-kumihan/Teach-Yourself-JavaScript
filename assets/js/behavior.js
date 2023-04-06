c0e1e485056a069d96297f00189399609c559424
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-04-06 15:57:36 +0900

    関数と引数 => form, input[type=text]でHTMLに入力を出力を実装。後半にエラー。argumentsメソッドは使わない。

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -12,3 +12,37 @@ function getTriangleArea(height, width) {
 reply.textContent = getTriangleArea(10, 5)
 console.log(`Ans: ${ getTriangleArea(10, 5) }`)
 
+// 引数チェック
+
+// // 入力をHTMLに出力する。
+// document.getElementById('form').onsubmit = function (event) {
+//   // preventDefault()を説明できるようになること。
+//   event.preventDefault();
+//   let inputText = document.querySelector("#argCheck").value
+//   document.getElementById("reply2").textContent = inputText
+// }
+
+// エラー処理と合せる。
+// 処理を書いたがエラーで思うように動かない。
+function showMessage(value) {
+  if (arguments.length !== 1) {
+    throw new Error(`引数が複数入っています。入力された引数：${ arguments.length }個`)
+  } else {
+    const reply2 = document.getElementById("reply2")
+    return reply2.textContent = `入力された引数は『${ value }』です。`
+  }
+}
+
+try {
+  document.getElementById("form").onsubmit = function (event) {
+    event.preventDefault()
+    let inputText = document.getElementById("argCheck").value
+    // ちゃんとテキストのクラス
+    console.log(typeof(inputText))
+    // 関数に引数はテキストクラスじゃないのか？
+    showMessage(inputText)
+  }
+  // e => 投げられたエラー『throw new Error()』のインスタンスが入っている。
+} catch(e) {
+  console.log(e.message)
+}
