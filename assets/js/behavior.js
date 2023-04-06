c4400c2cdca28b4053ac1d494d003bf021f77d14
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-04-07 08:46:53 +0900

    関数と可変長引数 => 入力した値を配列にして計算結果をHTMLに出力する。入力値判断の問題でエラー。
    	疑問：formに入力された値の判断について
    		入力をHTMLに出力する。
    		入力した値を配列に変更しエラー処理し、数字であれば合計して返す。
    		筋書きはこれでいいが大問題発生。
    		input[type = "text"]だから『数字』かそれ以外で条件分岐できない根本的な問題。
    		どのようにして解決しているのか？
    		なので、HTMLに出力をするコードは書いていない。

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,48 +1,47 @@
-let query = document.getElementById("query")
-let reply = document.getElementById("reply")
-
-query.textContent = "高さ10㎝、底辺5㎝の三角形の面積を求めなさい。"
-
-function getTriangleArea(height, width) {
-  return height * width / 2
+function sum(...nums) {
+  let result = 0
+  for (let num of nums) {
+    if (typeof num !== "number") {
+      throw new Error(`指定値が数値ではありません。：${ num }`)
+    }
+    result += num
+  }
+  return result 
 }
+let ans = document.getElementById("reply")
+ans.textContent = `合計は、${ sum(1,3,5,7,9) }となります。`
 
-// 関数を定義の利点とは、例えば、面積を求める場合、
-// ここの引数の値を入れ替えることで汎用性を高めることができるから。
-reply.textContent = getTriangleArea(10, 5)
-console.log(`Ans: ${ getTriangleArea(10, 5) }`)
-
-// 引数チェック
+console.log(sum(1,3,5,7,9))
 
 // // 入力をHTMLに出力する。
+// // 入力した値を配列に変更しエラー処理し、数字であれば合計して返す。
+// // 筋書きはこれでいいが大問題発生。
+// // input[type = "text"]だから『数字』かそれ以外で条件分岐できない根本的な問題。
+// // どのようにして解決しているのか？
+// // なので、HTMLに出力をするコードは書いていない。
+
+// function sum(array) {
+//   let result = 0
+//   for (let ins of array) {
+//     if (typeof(ins) !== "number") {
+//       throw new Error(`指定値が数値ではありません。：${ ins }`)
+//     }
+//     result += ins
+//   }
+//   return result
+// }
+
+// // formに入力があったら、値を配列に変更する。
 // document.getElementById('form').onsubmit = function (event) {
 //   // preventDefault()を説明できるようになること。
 //   event.preventDefault();
-//   let inputText = document.querySelector("#argCheck").value
-//   document.getElementById("reply2").textContent = inputText
+//   let inputText = document.querySelector("#argCheck").value.split(",")
+//   let tmpArray = []
+//   for (let ins of inputText) {
+//     if (typeof(ins) !== "number") {
+//       throw new Error(`入力された値は数値ではありません。:${ ins }`)
+//     }
+//     tmpArray.push(ins)
+//   }
+//   console.log(tmpArray)
 // }
-
-// エラー処理と合せる。
-// 処理を書いたがエラーで思うように動かない。
-function showMessage(value) {
-  if (arguments.length !== 1) {
-    throw new Error(`引数が複数入っています。入力された引数：${ arguments.length }個`)
-  } else {
-    const reply2 = document.getElementById("reply2")
-    return reply2.textContent = `入力された引数は『${ value }』です。`
-  }
-}
-
-try {
-  document.getElementById("form").onsubmit = function (event) {
-    event.preventDefault()
-    let inputText = document.getElementById("argCheck").value
-    // ちゃんとテキストのクラス
-    console.log(typeof(inputText))
-    // 関数に引数はテキストクラスじゃないのか？
-    showMessage(inputText)
-  }
-  // e => 投げられたエラー『throw new Error()』のインスタンスが入っている。
-} catch(e) {
-  console.log(e.message)
-}
