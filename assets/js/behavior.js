61aec2c12a9ba258489d2b34994375c5650c5722
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-04-11 10:32:08 +0900

    進捗確認 => .querySelector, .querySelectorAll, .getElementById, NodeWalking, Event-driven model, setAttribute("style", `value`)

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,94 +1,101 @@
-// // org: 可変長引数のサンプルコード
-// function sum(...nums) {
-//   let result = 0
-//   for (let num of nums) {
-//     if (typeof num !== "number") {
-//       throw new Error(`指定値が数値ではありません。：${ num }`)
-//     }
-//     result += num
-//   }
-//   return result 
-// }
-// let ans = document.getElementById("reply")
-// ans.textContent = `合計は、${ sum(1,3,5,7,9) }となります。`
+// 要素を取るやり方
+const dl = document.querySelector(".wrapper.one > dl")
 
+// HTMLCollectionを取る
+console.log(dl.children)
 
+// HTMLCollectionの子要素とその値を現す
+for (ins of dl.children) {
+  console.log(ins)
+  console.log(ins.textContent)
+}
 
-// // formに入力されたものは何か そして、それをどうしたいのか？
-// document.getElementById("form").onsubmit = function (event) {
-//   event.preventDefault()
-//   let inputTexts = document.getElementById("argCheck").value.split(",")
-//   console.log(inputTexts)
-//   // => ['1', '2', '3']
-//   console.log(typeof(inputTexts))
-//   // => object
-//   // イレラブルな動きをする。元の配列は変更しない。
-//   console.log(inputTexts.map(Number))
-//   // => [1, 2, 3]
-//   console.log(inputTexts.map(str => parseInt(str, 10)))
-//   // => [1, 1, 1]
-// }
+// NodeListを取る
+console.log(dl.childNodes)
 
-
-
-// // 入力に数字と文字列を混ぜる。HTMLに出力するコード。
-// document.getElementById("form").onsubmit = function (event) {
-//   event.preventDefault()
-//   let inputTexts = document.getElementById("argCheck").value.split(",")
-//   let ans = document.getElementById("reply")
-
-//   let sum = 0
-//   for (let ins of inputTexts.map(Number)) {
-//     if (isNaN(ins)) {
-//       ans.textContent = "配列の中に数字以外の文字が含まれています。合計できません。"
-//       break;
-//     } else {
-//       sum += ins
-//     }
-//   }
-//   ans.textContent = `合計は『${ sum }』です。`
-// }
-
-
-
-// // throw, try ~ catchを使ってみる。
-// function calc(value) {
-//   let result = 0
-//   for (let ins of value.map(Number)) {
-//     if (isNaN(ins)) {
-//       throw new Error(`数字ではないものが含まれています。合計できません。`)
-//     }
-//     result += ins
-//   }
-//   return result
-// }
-
-// document.getElementById("form").onsubmit = function (event) {
-//   event.preventDefault()
-//   let inputTexts = document.getElementById("argCheck").value.split(",")
-//   let reply = document.getElementById("reply")
-
-//   try {
-//     reply.textContent = `合計は『${ calc(inputTexts) }』です。`
-//   } catch(e) {
-//     reply.textContent = e.message
-//   }
-// }
-
-
-
-// Vue.jsを使ってみる。
-Vue.createApp({
-  data: function() {
-    return { value: "" }
-  },
-  methods: {
-    reply() {
-      let result = 0
-      for (ins of this.value.split(" ").map(Number)) {
-        result += ins
-      }
-      return result;
-    }
+// NodeListの子要素で要素ノードだけとその値を現す
+for (ins of dl.childNodes) {
+  if (ins.nodeType === 1) {
+    console.log(ins)
+    console.log(ins.textContent)
   }
-}).mount("#app")
+}
+
+// 要素を取る
+console.log(dl.parentElement)
+const nodeTest = document.querySelector(".nodeTest")
+console.log(nodeTest.nextElementSibling)
+// これも読みにくい
+console.log(dl.firstElementChild.nextElementSibling.lastElementChild.textContent)
+
+// Nodeウォーキング　最後の行はやりすぎで読みにくくなるだけ。
+const figure = document.getElementById("cover")
+console.log(figure.firstElementChild.alt)
+console.log(figure.firstElementChild.src)
+console.log(figure.firstElementChild.nextElementSibling.firstElementChild.href)
+
+// 画像にマウスをかざしたら別画像に切り替える。
+const pic = document.getElementById("cover")
+pic.addEventListener("mouseenter", function () {
+  this.firstElementChild.src = "../img/p2.jpg"
+}, false)
+
+// マウスが画像から別画像に切り替える。
+pic.addEventListener("mouseleave", function () {
+  this.firstElementChild.src = "../img/p3.jpg"
+}, false)
+
+// anchorをクリックしたら別窓でURLを開く。
+const link = document.getElementById("site")
+link.addEventListener("click", function () {
+  this.setAttribute("target", "_blank")
+}, false)
+
+// ----------------------------------------------------
+// 配列内の値の順をランダムにする関数
+function arrShuffle(array) {
+  // array.length - 1 => 最後の値のインデックス番号
+  for (let i = (array.length - 1); 0 < i; i--) {
+    // 0〜(i+1)未満の範囲でランダムな値を取得
+    let r = Math.floor(Math.random() * (i + 1));
+    let tmp = array[i];
+    array[i] = array[r];
+    array[r] = tmp;
+  }
+  return array;
+}
+
+// ターゲットとなる全てのリストを収集する。
+const container = document.querySelector(".grid-container")
+let lists = container.children
+
+container.addEventListener("mouseenter", function () {
+  // ランダムな順番を格納した配列を生成する。
+  let nums = []
+  for (let i = 1; i < lists.length + 1; i++) {
+    nums.push(i)
+  }
+  let ramdomNums = arrShuffle(nums)
+
+  // リストを最初から呼び出して、スタイルにランダムな数字を入れていく。
+  for (let i = 0; i < lists.length; i++) {
+    lists[i].setAttribute("style", `order:${ramdomNums[i]}`)
+  }
+})
+
+container.addEventListener("mouseleave", function () {
+  // ランダムな順番を格納した配列を生成する。
+  let nums = []
+  for (let i = 1; i < lists.length + 1; i++) {
+    nums.push(i)
+  }
+  let ramdomNums = arrShuffle(nums)
+
+  // リストを最初から呼び出して、スタイルにランダムな数字を入れていく。
+  for (let i = 0; i < lists.length; i++) {
+    lists[i].setAttribute("style", `order:${ramdomNums[i]}`)
+  }
+})
+
+
+
