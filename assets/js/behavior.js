b647f40ddbf5d37f9f5245e6fdc37c5fcd1e6a3d
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-04-24 11:54:35 +0900

    文章を分解し1文字ごとに<span>で囲む=> 1文字ごとにアニメーションをかけるための準備
    	Array.from(HTMLCollection or NodeList)
    		HTMLCollectionまたはNodeListを配列で扱えるようにする。
    		forEach()が使えるメリット。
    
    	replaceAll(pattern, repstr)を使って正規表現の検索置換ができた。

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,64 +1,9 @@
-// ファイル選択（複数可）
-let sendPkg = document.getElementById("sendPkg")
-let preWord = document.getElementById("preWord")
-let fileName = document.getElementById("fileName")
-
-sendPkg.addEventListener("change", function() {
-  let files = sendPkg.files
-  let tmpNames = []
-  for (let file of files) {
-    tmpNames.push(file.name)
-    console.log(file)
-    // console.log(file.type)
-    // console.log(new Date(file.lastModified))
-    // console.log(file.name)
-    // console.log(file.size)
-    // console.log(file.type)
-  }
-  preWord.textContent = "選択したファイルは"
-  fileName.textContent = tmpNames.join("／")
-}, false)
-
-// ------------------------------------------------------------
-
-// ファイル内容の表示
-let sendText = document.getElementById("sendText")
-let preText = document.getElementById("preText")
-let textTitle = document.getElementById("textTitle")
-let view = document.querySelector(".result")
-let reader = new FileReader()
-
-sendText.addEventListener("change", function() {
-  preText.textContent = "選択したファイルは"
-  textTitle.textContent = sendText.files[0].name
-
-  reader.addEventListener("load", function() {
-    view.classList.add("active")
-    view.innerText = reader.result
-    // view.textContent = reader.result
-  })
-
-  document.querySelector('input[type="button"]').addEventListener("click", function() {
-    reader.readAsText(sendText.files[0], 'utf-8')
-  })
+let paragraphs = Array.from(document.getElementById("article").children)
+paragraphs.forEach(p => {
+  tmpStr = ""
+  for (let char of p.textContent) {
+    tmpStr += `<span>${ char }</span>`
+  } 
+  const pattern = /<span>(。|、)<\/span>/g
+  p.innerHTML = tmpStr.replaceAll(pattern, '<span class="right-align">$1</span>')
 })
-
-// ------------------------------------------------------------
-
-// MDN sample
-// function previewFile() {
-//   const content = document.querySelector('.content');
-//   const [file] = document.querySelector('input[type=file]').files;
-//   const reader = new FileReader();
-
-//   reader.addEventListener("load", () => {
-//     // this will then display a text file
-//     content.innerText = reader.result;
-//   }, false);
-
-//   if (file) {
-//     reader.readAsText(file);
-//   }
-// }
-
-// ------------------------------------------------------------
