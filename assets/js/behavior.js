62b3a1acc702aea08ddf4a921f0a6c872a847439
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-04-27 16:54:01 +0900

    ランダムな値を出力してみる

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,6 +1,19 @@
 let arr = [1, 2, 3, 4, 5, 6]
+
+let randomNum = arr.sort(function () {
+  return Math.random() - 0.5
+})
+
+console.log(randomNum)
+
+
 let strArr = []
 arr.map(function (num) {
   this.push(String(num))
 }, strArr)
-console.log(strArr)
+
+let randomStr = strArr.sort(function () {
+  return Math.random() - 0.5
+})
+
+console.log(randomStr)
