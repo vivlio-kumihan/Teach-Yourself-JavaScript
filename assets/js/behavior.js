56cb822bd218110086ceb2803b61881eef1239d3
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-05-01 09:12:21 +0900

    scroll => クリックする度にスクリーン1面分ごと上げる

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,11 +1,19 @@
-// 通常画面と全画面を切り替える
-document.getElementById("btn").addEventListener("click", function() {
-  if (this.value === "全画面") {
-    this.setAttribute("value", "戻る")
-    document.body.requestFullscreen()
-  } else {
-    this.setAttribute("value", "全画面")
-    document.exitFullscreen()
-  }
+// クリックする度にスクリーン1面分ごと上げる
+document.getElementById("container").addEventListener("click", function() {
+  // scrollBy() => 指定した位置までスクロールする。
+  // X軸は0、Y軸がブラウザに写っていて、ブラウザのヘッダー以下の画面。
+  window.scrollBy(0, window.innerHeight)
 }, false)
+
+
+// // 通常画面と全画面を切り替える
+// document.getElementById("btn").addEventListener("click", function() {
+//   if (this.value === "全画面") {
+//     this.setAttribute("value", "戻る")
+//     document.body.requestFullscreen()
+//   } else {
+//     this.setAttribute("value", "全画面")
+//     document.exitFullscreen()
+//   }
+// }, false)
 
