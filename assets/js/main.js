9bdc7719afafd45a833c8f86683ebb87325c8e14
Nobuyuki Takahiro <vivlio@kumihan.com>
2024-07-04 06:17:48 +0900

    try error finally

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,34 @@
 
 
-// container.addEventListener("mouseenter", () => {
-//   const bgColor = container.getAttribute("data-mouseenter")
-//   container.style.backgroundColor = bgColor;
-//   });
+// // container.addEventListener("mouseenter", () => {
+// //   const bgColor = container.getAttribute("data-mouseenter")
+// //   container.style.backgroundColor = bgColor;
+// //   });
   
-// container.addEventListener("mouseleave", () => {
-//   container.style.backgroundColor = "";
-// });
+// // container.addEventListener("mouseleave", () => {
+// //   container.style.backgroundColor = "";
+// // });
     
-// target.addEventListener("mouseenter", () => {
-//   const bgColor = target.getAttribute("data-mouseenter")
-//   target.style.backgroundColor = bgColor;
-// });
+// // target.addEventListener("mouseenter", () => {
+// //   const bgColor = target.getAttribute("data-mouseenter")
+// //   target.style.backgroundColor = bgColor;
+// // });
 
-// target.addEventListener("mouseleave", () => {
-//   target.style.backgroundColor = "";
-// });
+// // target.addEventListener("mouseleave", () => {
+// //   target.style.backgroundColor = "";
+// // });
 
-const container = document.querySelector("#container");
-const target = document.querySelector("#target");
+// const container = document.querySelector("#container");
+// const target = document.querySelector("#target");
 
-const setEvent = (elem) => {
-  const dataSets = Object.entries(elem.dataset);
-  for (const [entryType, color] of dataSets) {
-    elem.addEventListener(entryType, () => {
-      elem.style.backgroundColor = color;
-    });
-  }
-};
+// const setEvent = (elem) => {
+//   const dataSets = Object.entries(elem.dataset);
+//   for (const [entryType, color] of dataSets) {
+//     elem.addEventListener(entryType, () => {
+//       elem.style.backgroundColor = color;
+//     });
+//   }
+// };
 
-setEvent(container);
-setEvent(target);
+// setEvent(container);
+// setEvent(target);
